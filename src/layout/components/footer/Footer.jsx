1eb46e0872172f8a6229ddac3a953bc671e9b9f9
Zainal Arifin <arifin.1602@gmail.com>
2023-09-08 00:43:52 +0700

    draft footer

--- src/layout/components/footer/Footer.jsx
+++ src/layout/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import IconTwitter from '../../../assets/image/media-social/twt.svg';
 export default function Footer() {
   return (
     <Container className='footer-section' >
-      <Row>
+      <Row style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
         <Col md="6" >
           <Row>
             <Col><img src={IconFacebook} className='media-social-icon' /></Col>
@@ -15,7 +15,9 @@ export default function Footer() {
             <Col><img src={IconTwitter} className='media-social-icon' /></Col>
           </Row>
         </Col>
-        <Col md="6" ></Col>
+        <Col md="6" style={{ color: 'white', alignContent: 'center' }} >
+          Terms & Condition | Copyright &copy; 2018. All rights reserved. PT Radya Gita Bahagi
+        </Col>
       </Row>
     </Container>
   );
