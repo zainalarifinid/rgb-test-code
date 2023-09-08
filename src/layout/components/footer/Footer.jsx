7e53ee13ff607ef2914201b11bb99fb69aa19e32
Zainal Arifin <arifin.1602@gmail.com>
2023-09-08 23:17:32 +0700

    fixing footer

--- src/layout/components/footer/Footer.jsx
+++ src/layout/components/footer/Footer.jsx
@@ -6,10 +6,10 @@ import IconTwitter from '../../../assets/image/media-social/twt.svg';
 
 export default function Footer() {
   return (
-    <Container className='footer-section' >
-      <Row style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
-        <Col md="6" >
-          <Row>
+    <Container className='footer-section' style={{ display: 'flex' }} >
+      <Row style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }} >
+        <Col md="6"  >
+          <Row style={{ width: '300px' }} >
             <Col><img src={IconFacebook} className='media-social-icon' /></Col>
             <Col><img src={IconInstagram} className='media-social-icon' /></Col>
             <Col><img src={IconTwitter} className='media-social-icon' /></Col>
